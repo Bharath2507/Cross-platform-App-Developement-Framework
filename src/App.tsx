20a1e438e020e85ca4068f03bbfbc5e0aaa5f9af
pavuluri1999 <pavuluri1999@gmail.com>
2025-05-10 17:54:02 +0530

    Changes to deploy the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage'; // your existing component
-import ReportPage from './ReportPage'; // new component
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import ReportPage from './ReportPage';
 
 function App() {
   return (
-    <Router>
-      <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/report" element={<ReportPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/report" element={<ReportPage />} />
+    </Routes>
   );
 }
 
