fc28269f0a03f2c61c6f8bac9900c908a669b67a
pavuluri1999 <pavuluri1999@gmail.com>
2025-05-10 13:21:45 +0530

    GitHub pages prep

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace with your GitHub repo name
+const repoName = 'Cross-platform-App-Developement-Framework';
+
 export default defineConfig({
+  base: `/${repoName}/`, // 👈 Important for GitHub Pages
   plugins: [react()],
-})
+});
